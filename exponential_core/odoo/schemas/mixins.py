b99e6178357abd19dce26f54112909815fd79834
JohnRSantamaria <santamariajohnd@gmail.com>
2025-06-27 12:56:13 -0500

    fix: flied_anotation

--- exponential_core/odoo/schemas/mixins.py
+++ exponential_core/odoo/schemas/mixins.py
@@ -1,14 +1,15 @@
 # exponential_core/schemas/mixins.py
 
 from pydantic import field_validator
-from typing import Any, Optional
+from typing import Any
 
 
 class StrNormalizerMixin:
     @field_validator("*", mode="before")
     @classmethod
     def normalize_optional_strings(cls, v: Any, info):
-        if info.field_annotation in [str, Optional[str]]:
+        # Usa field_type en lugar de field_annotation (Pydantic v2)
+        if info.field_type is str:
             if isinstance(v, str) and not v.strip():
                 return None
         return v
