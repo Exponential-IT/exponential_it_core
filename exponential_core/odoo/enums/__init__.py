94a6f48666c1f8bb3d321d4322fd6ba403d03fa6
JohnRSantamaria <santamariajohnd@gmail.com>
2025-06-27 12:23:23 -0500

    udapte in imports

---
+++ exponential_core/odoo/enums/__init__.py
@@ -0,0 +1,13 @@
+# exponential_core/odoo/enums/__init__.py
+
+from .address import AddressTypeEnum
+from .company import CompanyTypeEnum
+from .product import ProductTypeEnum
+from .tax import TaxUseEnum
+
+__all__ = [
+    "AddressTypeEnum",
+    "CompanyTypeEnum",
+    "ProductTypeEnum",
+    "TaxUseEnum",
+]
---
+++ exponential_core/odoo/schemas/__init__.py
@@ -0,0 +1,14 @@
+# exponential_core/odoo/schemas/__init__.py
+
+from .invoice import InvoiceCreateSchema, InvoiceLineSchema
+from .product import ProductCreateSchema
+from .supplier import SupplierCreateSchema
+from .partnet_address import AddressCreateSchema
+
+__all__ = [
+    "InvoiceCreateSchema",
+    "InvoiceLineSchema",
+    "ProductCreateSchema",
+    "SupplierCreateSchema",
+    "AddressCreateSchema",
+]
