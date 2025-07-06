135c86f6329e561325540ff31646b055ab29af8a
JohnRSantamaria <santamariajohnd@gmail.com>
2025-07-06 13:22:57 -0500

    fix(secrets): manejar errores específicos de boto3 como ResourceNotFound y ResourceExists

--- exponential_core/secrets/aws_error_handler_async.py
+++ exponential_core/secrets/aws_error_handler_async.py
@@ -1,6 +1,10 @@
 import functools
 from botocore.exceptions import ClientError
-from exponential_core.exceptions import AWSConnectionError
+from exponential_core.exceptions import (
+    AWSConnectionError,
+    SecretNotFoundError,
+    SecretAlreadyExistsError,
+)
 
 
 def handle_boto3_errors_async(func):
@@ -9,5 +13,16 @@ def handle_boto3_errors_async(func):
         try:
             return await func(*args, **kwargs)
         except ClientError as e:
+            error_code = e.response.get("Error", {}).get("Code")
+            self_instance = args[0]
+            secret_name = getattr(self_instance, "base_secret_name", "desconocido")
+
+            if error_code == "ResourceNotFoundException":
+                raise SecretNotFoundError(secret_name=secret_name) from e
+
+            elif error_code == "ResourceExistsException":
+                raise SecretAlreadyExistsError(secret_name=secret_name) from e
+
             raise AWSConnectionError(str(e)) from e
+
     return wrapper
