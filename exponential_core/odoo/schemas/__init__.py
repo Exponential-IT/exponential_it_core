822481592a64b76a75e1ce4f9a08389e1753ecda
JohnRSantamaria <santamariajohnd@gmail.com>
2025-07-11 15:50:28 -0500

    update imports

--- exponential_core/odoo/schemas/__init__.py
+++ exponential_core/odoo/schemas/__init__.py
@@ -1,7 +1,7 @@
 # exponential_core/odoo/schemas/__init__.py
 
 from .invoice import InvoiceCreateSchema, InvoiceLineSchema
-from .product import ProductCreateSchema
+from .product import ProductCreateSchema, ProductCreateSchemaV18
 from .supplier import SupplierCreateSchema
 from .partnet_address import AddressCreateSchema
 from .taxes import ResponseTaxesSchema
@@ -10,6 +10,7 @@ __all__ = [
     "InvoiceCreateSchema",
     "InvoiceLineSchema",
     "ProductCreateSchema",
+    "ProductCreateSchemaV18",
     "SupplierCreateSchema",
     "AddressCreateSchema",
     "ResponseTaxesSchema",
